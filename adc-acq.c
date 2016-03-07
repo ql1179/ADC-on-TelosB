bbe770994e20343bee4dbe9ebf31f4bd05ec213f
ql1179 <qlin1179@gmail.com>
2016-03-07 11:01:05 +1100

    Added files via upload

--- adc-acq.c
+++ adc-acq.c
@@ -23,92 +23,47 @@ PROCESS_THREAD(adc_acq,ev,data)
       static int r;
       static uint8_t seq;
       static unsigned record[2];
-      static unsigned rread[2];
+
 
       PROCESS_BEGIN();
 	seq = 0;
       #if NEED_FORMATTING
 	cfs_coffee_format();
       #endif	      
-      cfs_remove(FILENAME);
-//write
-      fd = cfs_open(FILENAME, CFS_WRITE + CFS_APPEND + CFS_READ);
-      if(fd < 0) {
-	printf("failed to open %s\n", FILENAME);
-	return 0;
-      }   
-      record[0] = 52455;
-      record[1] = 0;
-      r = cfs_write(fd, record, sizeof(record));
-      if (r != sizeof(record)) {
-	printf("failed to write %d bytes to %s\n", (int)sizeof(record), FILENAME);
-	cfs_close(fd);
-	return 0;
-      }
-      printf("write success %u\n", record[0]);
-      cfs_close(fd);
-
-//append
-      fd = cfs_open(FILENAME, CFS_WRITE + CFS_APPEND + CFS_READ);
-      if(fd < 0) {
-	printf("failed to open %s\n", FILENAME);
-	return 0;
-      }   
-      record[0] = 524;
-      record[1] = 0;
-      r = cfs_write(fd, record, sizeof(record));
-      if (r != sizeof(record)) {
-	printf("failed to write %d bytes to %s\n", (int)sizeof(record), FILENAME);
-	cfs_close(fd);
-	return 0;
-      }
-      printf("append success %u\n", record[0]);
-      cfs_close(fd);
-
-
-//read
-      fd = cfs_open(FILENAME, CFS_READ);
-      if(fd < 0) {
-	printf("failed to open %s\n", FILENAME);
-	return 0;
-      }
-      if (cfs_seek(fd, 0, CFS_SEEK_SET) != 0) {
-	printf("seek failed\n");
-        cfs_close(fd);
-	return 0;
-      }
-	printf("file opened\n");
-
-      for(;;){
-        r = cfs_read(fd, rread, sizeof(rread));
-	printf("seq %d\n", seq);
-	seq++;
-        if (r == 0) {
-          printf("read finish\n");
-          break;
-        }
-        else if (r != sizeof(rread)) {
-	  printf("failed to read %d bytes to %s\n", (int)sizeof(rread), FILENAME);
-	  cfs_close(fd);
-	  return 0;
-        }
-        printf("Read success %u\n", rread[0]);
-      }
-      cfs_close(fd);
-      cfs_remove(FILENAME);
 
 
 
-      while(1)
+
+
+
+
+
+      while(seq <= 100000)
       {
 	   etimer_set(&et, CLOCK_SECOND * 0.1);
 	   SENSORS_ACTIVATE(light_sensor);
 	   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
+	   seq++;
 
            val = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
-//      	   if(val != -1) 
-//		printf("read value = %d\n", val);                         
+      	   if(val != -1) {
+		//write
+	      fd = cfs_open(FILENAME, CFS_WRITE + CFS_APPEND + CFS_READ);
+	      if(fd < 0) {
+		printf("failed to open %s\n", FILENAME);
+		return 0;
+      	      }   
+      	      record[0] = val;
+              record[1] = 0;
+              r = cfs_write(fd, record, sizeof(record));
+              if (r != sizeof(record)) {
+	        printf("failed to write %d bytes to %s\n", (int)sizeof(record), FILENAME);
+	        cfs_close(fd);
+	        return 0;
+              }
+              printf("write success %u\n", record[0]);
+              cfs_close(fd);
+           }                      
 	   etimer_reset(&et);
     	   SENSORS_DEACTIVATE(light_sensor);
       } //end of while
