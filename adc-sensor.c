0d77e73d4f8a21fd526636dc0ccd885ff33b0ba5
ql1179 <qlin1179@gmail.com>
2016-03-15 09:56:06 +1100

    Added files via upload

--- adc-sensor.c
+++ adc-sensor.c
@@ -1,32 +1,94 @@
 #include "contiki.h"
 #include "lib/sensors.h"
-#include "dev/sky-sensors.h"
 #include "dev/adc-sensor.h"
-
-#define INPUT_CHANNEL      1 << INCH_0
-#define INPUT_REFERENCE     SREF_0
-#define DATA_MEM  ADC12MEM0
 
 const struct sensors_sensor adc_sensor;
 
 /*---------------------------------------------------------------------------*/
-static int
-value(int type)
+static CC_INLINE void
+start(void)
 {
-  return DATA_MEM;
+
+  /* Set up the ADC. */
+  P6DIR = 0xff;
+  P6OUT = 0x00;
+
+  /* Setup ADC12, ref., sampling time */
+  /* XXX Note according to the specification a minimum of 17 ms should
+     be allowed after turn on of the internal reference generator. */
+ // ADC12CTL0 = REF2_5V + SHT0_6 + SHT1_6 + MSC + REFON;
+  ADC12CTL0 = SHT0_6 + SHT1_6 + MSC;
+  /* Use sampling timer, repeat-sequence-of-channels */
+  ADC12CTL1 =  SHP + CONSEQ_2 + CSTARTADD_0;
+
+//  /* Set highest end-of-sequence. */
+//  ADC12MCTL0 = EOS;
+
+  ADC12CTL0 |= ADC12ON;
+  ADC12CTL0 |= ENC;                   /* enable conversion */
+  ADC12CTL0 |= ADC12SC;               /* sample & convert */
 }
 /*---------------------------------------------------------------------------*/
-static int
-configure(int type, int c)
+static CC_INLINE void
+stop(void)
 {
-  return sky_sensors_configure(INPUT_CHANNEL, INPUT_REFERENCE, type, c);
+  /* stop converting immediately, turn off reference voltage, etc. */
+
+  ADC12CTL0 &= ~ENC;
+  /* need to remove CONSEQ_3 if not EOS is configured */
+  ADC12CTL1 &= ~CONSEQ_2;
+
+  /* wait for conversion to stop */
+  while(ADC12CTL1 & ADC12BUSY);
+
+  /* clear any pending interrupts */
+  ADC12IFG = 0;
 }
 /*---------------------------------------------------------------------------*/
 static int
 status(int type)
 {
-  return sky_sensors_status(INPUT_CHANNEL, type);
+  if(type == SENSORS_ACTIVE) {
+    return 1;
+  }
+  if(type == SENSORS_READY) {
+    return (ADC12IFG & INCH_0) == INCH_0;
+  }
+  return 0;
 }
+/*---------------------------------------------------------------------------*/
+static int
+configure(int type, int c)
+{
+
+  if(type == SENSORS_ACTIVE) {
+    stop();
+
+    if(c) {
+      P6SEL |= INCH_0 & 0xff;
+
+      /* Set ADC config */
+      ADC12MCTL0 = INCH_0 + SREF_0;
+
+    } 
+    else {
+      P6SEL &= ~(INCH_0 & 0xff);
+    }
+
+
+    start();
+    return 1;
+  }
+  return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+static int
+value(int type)
+{
+  return ADC12MEM0 & 0x0FFF;
+}
+
 /*---------------------------------------------------------------------------*/
 SENSORS_SENSOR(adc_sensor, "ADC_SENSOR",
                value, configure, status);
