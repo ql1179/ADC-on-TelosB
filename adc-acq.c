699271c08f42e4d622f4271a9b5edf72ed5564dd
ql1179 <qlin1179@gmail.com>
2016-03-15 09:56:35 +1100

    Added files via upload

--- adc-acq.c
+++ adc-acq.c
@@ -1,6 +1,5 @@
 #include "contiki.h"
 #include "lib/sensors.h"
-#include "dev/sky-sensors.h"
 #include "dev/adc-sensor.h"
 #include "cfs/cfs.h"
 #include "cfs/cfs-coffee.h"
@@ -21,31 +20,34 @@ PROCESS_THREAD(adc_acq,ev,data) {
       static int val;
       static int fd;
       static int r;
-      static uint8_t seq; // sample seq
+      static unsigned long seq; // sample seq
       static unsigned record[2]; // record[0] is the actual reading, record[1] is zero padding. Coffee system requires 1 byte zero padding between data. We just simply use 2 byte integer type of zero for padding.
 
 
       PROCESS_BEGIN();
       seq = 0;
+      ADC12IE = 0;
       #if NEED_FORMATTING
 	cfs_coffee_format();
       #endif	      
       cfs_remove(FILENAME);
-
+      fd = cfs_open(FILENAME, CFS_WRITE + CFS_APPEND + CFS_READ);
+      if(fd < 0) {
+	printf("failed to open %s\n", FILENAME);
+	return 0;
+      }   
+      SENSORS_ACTIVATE(adc_sensor);
       while(seq <= 200000) { // sampling will terminate when reach 200, 000 
 	   etimer_set(&et, CLOCK_SECOND * 0.01); // time interval is 0.01, which is 100Hz.
-	   SENSORS_ACTIVATE(adc_sensor);
+	//   SENSORS_ACTIVATE(adc_sensor);
 	   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 	   seq++;
+	   
 
            val = adc_sensor.value(0);
       	   if(val != -1) {
 		//write
-	      fd = cfs_open(FILENAME, CFS_WRITE + CFS_APPEND + CFS_READ);
-	      if(fd < 0) {
-		printf("failed to open %s\n", FILENAME);
-		return 0;
-      	      }   
+	      
       	      record[0] = val; // 12bit reading
               record[1] = 0; // zero padding
               r = cfs_write(fd, record, sizeof(record));
@@ -55,12 +57,15 @@ PROCESS_THREAD(adc_acq,ev,data) {
 	        return 0;
               }
               printf("write success %u\n", record[0]);
-              cfs_close(fd);
-           }                      
+              
+           }  
+
+ //        SENSORS_DEACTIVATE(adc_sensor);                    
 	   etimer_reset(&et);
-    	   SENSORS_DEACTIVATE(adc_sensor);
+
       } //end of while
-    
+      cfs_close(fd);  
+	SENSORS_DEACTIVATE(adc_sensor);  
       PROCESS_END();
 
 }
